96fe0ba28eaf9a2b138c5e647a40b2e2f691393a
Gurjas Singh <gsingh.ont@gmail.com>
2023-04-10 16:57:39 -0400

    interval dp questions

---
+++ Dmoj/ DMOPC '14 Contest 4/P3 - Golden Lily.cpp
@@ -0,0 +1,81 @@
+/********   All Required Header Files ********/
+#include <array>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <sstream>
+#include <queue>
+#include <deque>
+#include <bitset>
+#include <iterator>
+#include <list>
+#include <stack>
+#include <map>
+#include <set>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <limits>
+#include <time.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <limits>
+#include <map>
+#include <unordered_map>
+#include <stdlib.h>
+#include <assert.h>
+#include <cmath>
+
+using namespace std;
+
+/* TYPES */
+#define ll long long
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define vi vector<int>
+#define vll vector<ll>
+#define mii map<int, int>
+
+/* UTILS */
+#define fi first
+#define se second
+#define pb push_back
+#define eb emplace_back
+#define MOD 1000000007
+#define PI 3.1415926535897932384626433832795
+#define read(type) readInt<type>()
+ll min(ll a,int b) { if (a<b) return a; return b; }
+ll min(int a,ll b) { if (a<b) return a; return b; }
+ll max(ll a,int b) { if (a>b) return a; return b; }
+ll max(int a,ll b) { if (a>b) return a; return b; }
+ll gcd(ll a,ll b) { if (b==0) return a; return gcd(b, a%b); }
+ll lcm(ll a,ll b) { return a/gcd(a,b)*b; }
+string to_upper(string a) { for (int i=0;i<(int)a.size();++i) if (a[i]>='a' && a[i]<='z') a[i]-='a'-'A'; return a; }
+string to_lower(string a) { for (int i=0;i<(int)a.size();++i) if (a[i]>='A' && a[i]<='Z') a[i]+='a'-'A'; return a; }
+bool prime(ll a) { if (a==1) return 0; for (int i=2;i<=round(sqrt(a));++i) if (a%i==0) return 0; return 1; }
+void yes() { cout<<"YES\n"; }
+void no() { cout<<"NO\n"; }
+
+const int MM = 1e5 + 5;
+
+int h, w, dp[1005][1005], grid[1005][1005];
+
+int main() {
+    cin >> w >> h;
+    memset(dp, 0x3f, sizeof dp);
+    dp[0][1] = 0;
+    for (int i = 1; i<=h; i++) {
+        for (int j = 1; j<= w; j++) {
+            cin >> grid[i][j];
+            dp[i][j] = min(dp[i-1][j], dp[i][j-1]) + grid[i][j];
+        }
+        for (int j = w; j>=1; j--) {
+            dp[i][j] = min(dp[i][j], dp[i][j+1]+grid[i][j]);
+        }
+    }
+    int x; int y; cin >> x >> y;
+    cout << dp[y+1][x+1] << endl;
+}
---
+++ Dmoj/Educational DP Contest AtCoder/H - Grid 1.cpp
@@ -0,0 +1,76 @@
+/********   All Required Header Files ********/
+#include <array>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <sstream>
+#include <queue>
+#include <deque>
+#include <bitset>
+#include <iterator>
+#include <list>
+#include <stack>
+#include <map>
+#include <set>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <limits>
+#include <time.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <limits>
+#include <map>
+#include <unordered_map>
+#include <stdlib.h>
+#include <assert.h>
+#include <cmath>
+
+using namespace std;
+
+/* TYPES */
+#define ll long long
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define vi vector<int>
+#define vll vector<ll>
+#define mii map<int, int>
+
+/* UTILS */
+#define fi first
+#define se second
+#define pb push_back
+#define eb emplace_back
+#define MOD 1000000007
+#define PI 3.1415926535897932384626433832795
+#define read(type) readInt<type>()
+ll min(ll a,int b) { if (a<b) return a; return b; }
+ll min(int a,ll b) { if (a<b) return a; return b; }
+ll max(ll a,int b) { if (a>b) return a; return b; }
+ll max(int a,ll b) { if (a>b) return a; return b; }
+ll gcd(ll a,ll b) { if (b==0) return a; return gcd(b, a%b); }
+ll lcm(ll a,ll b) { return a/gcd(a,b)*b; }
+string to_upper(string a) { for (int i=0;i<(int)a.size();++i) if (a[i]>='a' && a[i]<='z') a[i]-='a'-'A'; return a; }
+string to_lower(string a) { for (int i=0;i<(int)a.size();++i) if (a[i]>='A' && a[i]<='Z') a[i]+='a'-'A'; return a; }
+bool prime(ll a) { if (a==1) return 0; for (int i=2;i<=round(sqrt(a));++i) if (a%i==0) return 0; return 1; }
+void yes() { cout<<"YES\n"; }
+void no() { cout<<"NO\n"; }
+
+const int MM = 1e5 + 5;
+
+int h, w, grid[1005][1005]; char idk;
+
+int main() {
+    cin >> h >> w;
+    grid[0][1] = 1;
+    for (int i = 1; i<=h; ++i) {
+        for (int j = 1; j<= w; ++j) {
+            cin >> idk;
+            if (idk != '#') grid[i][j] = grid[i-1][j] + grid[i][j-1];
+        }
+    }
+    cout << grid[h][w] % MOD;
+}
