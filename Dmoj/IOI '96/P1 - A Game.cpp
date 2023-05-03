71b0ca29780b868edb993a485a7ae6b4c154b1fe
Gurjas Singh <gsingh.ont@gmail.com>
2023-05-03 16:14:36 -0400

    inteval dp ioi a game 10pt

---
+++ Dmoj/IOI '96/P1 - A Game.cpp
@@ -0,0 +1,81 @@
+// https://dmoj.ca/problem/ioi96p1
+// IOI '96 P1 - A Game
+
+/********   All Required Header Files ********/
+#include <array>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <sstream>
+#include <queue>
+#include <deque>
+#include <bitset>
+#include <iterator>
+#include <list>
+#include <stack>
+#include <map>
+#include <set>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <limits>
+#include <time.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <limits>
+#include <map>
+#include <unordered_map>
+#include <stdlib.h>
+#include <assert.h>
+#include <cmath>
+
+using namespace std;
+
+/* TYPES */
+#define ll long long
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define vi vector<int>
+#define vll vector<ll>
+#define mii map<int, int>
+
+/* UTILS */
+#define fi first
+#define se second
+#define pb push_back
+#define eb emplace_back
+#define MOD 1000000007
+#define PI 3.1415926535897932384626433832795
+#define read(type) readInt<type>()
+ll min(ll a,int b) { if (a<b) return a; return b; }
+ll min(int a,ll b) { if (a<b) return a; return b; }
+ll max(ll a,int b) { if (a>b) return a; return b; }
+ll max(int a,ll b) { if (a>b) return a; return b; }
+ll gcd(ll a,ll b) { if (b==0) return a; return gcd(b, a%b); }
+ll lcm(ll a,ll b) { return a/gcd(a,b)*b; }
+string to_upper(string a) { for (int i=0;i<(int)a.size();++i) if (a[i]>='a' && a[i]<='z') a[i]-='a'-'A'; return a; }
+string to_lower(string a) { for (int i=0;i<(int)a.size();++i) if (a[i]>='A' && a[i]<='Z') a[i]+='a'-'A'; return a; }
+bool prime(ll a) { if (a==1) return 0; for (int i=2;i<=round(sqrt(a));++i) if (a%i==0) return 0; return 1; }
+void yes() { cout<<"YES\n"; }
+void no() { cout<<"NO\n"; }
+
+const int MM = 105;
+int dp[MM][MM];
+int psa[MM];
+int N;
+
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0);
+    cin >> N;
+    for (int i = 1; i<=N; i++) {cin >> psa[i]; dp[i][i] = psa[i]; psa[i] += psa[i-1];}
+    for (int len = 1; len < N; len++) {
+        for (int l = 1, r = l + len; r <= N; l++, r++) {
+            dp[l][r] = psa[r] - psa[l-1] - min(dp[l+1][r], dp[l][r-1]);
+        }
+    }
+
+    cout << dp[1][N] << " " << psa[N] - dp[1][N]<< endl;
+}
