e28686a527a8fd1bfd9324ce364b30ea89a1fa5a
Gurjas Dhillon <gsingh.ont@gmail.com>
2023-08-21 23:19:01 -0400

    cross country

---
+++ Dmoj/Bluebook/Cross Country.py
@@ -0,0 +1,5 @@
+arr = {"MG" : "midget girls", "MB" : "midget boys", "JG" : "junior girls", "JB" : "junior boys", "SG" : "senior girls", "SB" : "senior boys"}
+x = input()
+
+if x in arr: print(arr[x])
+else: print("invalid code")
